85b4cc3e0c19c3ee18b0d4b681d290d4074ef7cf
Igor Golodnitsky <igor.golodnitsky@gmail.com>
2024-03-24 15:25:37 +0200

    - removed babel, not needed - webpack upgrade to 5, to use minicss plugin - css and js separated, to save 5kb of size in bundle (addStyles, urls)

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: 'babel-loader',
-            query: {
-              presets: [ '@babel/preset-env' ],
-            },
-          },
           'eslint-loader'
         ]
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           {
             loader: 'postcss-loader',
@@ -34,7 +36,14 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        loader: 'svg-inline-loader?removeSVGTagAttrs=false'
+        use: [
+          {
+            loader: 'svg-inline-loader',
+            options: {
+              removeSVGTagAttrs: false
+            }
+          }
+        ]
       }
     ]
   },
